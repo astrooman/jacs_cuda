0ef07635d1064d67f8d1b6fbbd047f3f0151250d
Mateusz Malenta <mateusz.malenta@gmail.com>
2016-05-04 15:19:20 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Examples for the CUDA JACS session.
---
+++ hello.cu
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cuda.h>
+
+using std::cout;
+using std::endl;
+
+__global__ void my_kernel()
+{
+    // I do absolutely nothing
+}
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello world!! I will call a CUDA kernel now!!" << endl;
+    my_kernel<<<1,1,0>>>();
+
+    return 0;
+}
