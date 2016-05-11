3c75506731e612c8ac6d2c3d0c703e033fe6a877
Mateusz Malenta <mateusz.malenta@gmail.com>
2016-05-11 22:47:56 +0100

    Added extra stuff

--- multiply_me.cu
+++ multiply_me.cu
@@ -1,16 +1,39 @@
 #include <chrono>
 #include <iostream>
+#include <random>
 #include <cuda.h>
 
 using std::cout;
 using std::endl;
 
-__global__ multiply_me_GPU(int *a, int *b, int *c) {
+__global__ multiply_me_GPU(int *a, int *b, int *c, int width) {
 
+    int row = blockIdx.y * gridDim.y + threadIdx.y;
+    int column = blockIdx.x * gridDim.x + threadIdx.x;
+
+    int sum = 0;
+
+    for (int ii = 0; ii < width; ii++) {
+        sum += a[row * width + ii] * b[column + ii * width];
+    }
+
+    c[row * width + column] = sum;
 
 }
 
-multiply_me_CPU(int *a, int *b, int *c) {
+multiply_me_CPU(int *a, int *b, int *c, int width) {
+
+    int sum;
+
+    for (int row = 0; row < width; row++) {
+        for (int column = 0; column < width; column++) {
+            sum = 0;
+            for (int k = 0; k < width; k++) {
+                sum += a[row * width + kk] * b[column + kk * width];
+            }
+            c[row * width + column] = sum;
+        }
+    }
 
 }
 
@@ -18,21 +41,28 @@ multiply_me_CPU(int *a, int *b, int *c) {
 int main(int argc, char *argv[])
 {
 
-    int N = 2048;
+    int N = 1024;
 
     int *h_a = new int[N * N];
     int *h_b = new int[N * N];
     int *h_c = new int[N * N];
     int *h_c2 = new int [N * N];
 
-    for (int ii = 0; ii < N; ii++) {
-        h_a[ii] = ii;
-        h_b[ii] = 2 * ii;
+    unsigend int seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::mt19937 engine(seed);
+    std::uniform_int_distribution<int> distribution(0, 50);
+
+    for (int ii = 0; ii < N * N; ii++) {
+        h_a[ii] = distribution(engine);
+        h_b[ii] = distribution(engine);
     }
 
     int *d_a;
     int *d_b;
     int *d_c;
+
+    dim3 nblocks (32,32,1);
+    dim3 nthreads(N/nblocks.x, N/nblocks.y, 1);
 
     float copy_elapsed;
     cudaEvent_t copy_start;
@@ -58,7 +88,7 @@ int main(int argc, char *argv[])
     cudaEventCreate(&GPU_stop);
 
     cudaEventRecord(GPU_start, 0);
-    multiply_me_GPU<<<1, 256>>>(d_a, d_b, d_c);
+    multiply_me_GPU<<<nblocks, nthreads>>>(d_a, d_b, d_c, N);
     cudaEventRecord(GPU_stop, 0);
     cudaEventSynchronise(GPU_stop);
     cudaElapsedTime(&GPU_elapsed, GPU_start, GPU_stop)
@@ -69,7 +99,7 @@ int main(int argc, char *argv[])
     std::chrono::duration<double> CPU_elapsed;
 
     CPU_start = std::chrono::system_clock::now();
-    multiply_me_CPU(h_a, h_b, h_c2);
+    multiply_me_CPU(h_a, h_b, h_c2, N);
     CPU_stop = std::chrono::system_clock::now();
     CPU_elapsed = CPU_start - CPU_stop;
 
---
+++ quick_fft.cu
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cufft.h>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[])
+{
+
+    cufftComplex sig1 = new cufftComplex[8];
+    cufftComplex sig2 = new cufftComplex[8];
+    cufftComplex sig3 = new cufftComplex[8];
+
+    for (int ii = 0; ii < 8; ii ++) {
+        sig1[ii].x = 1.0f * (ii % 2);
+        sig1[ii].y = 0.0f;
+        sig2[ii].x = 0.0f;
+        sig2[ii].y = -1.0f * (ii % 2);
+        sig3[ii].x = sig1[ii].x;
+        sig3[ii].y = sig2[ii].y
+
+    }
+
+    cufftComplex *d_s1;
+    cufftComplex *d_s2;
+    cufftComplex *d_s3;
+
+    cudaMalloc((void**)&d_s1, 8 * sizeof(cufftCoplex));
+    cudaMalloc((void**)&d_s2, 8 * sizeof(cufftCoplex));
+    cudaMalloc((void**)&d_s3, 8 * sizeof(cufftCoplex));
+
+    cudaMemcpy(d_s1, sig1, 8 * sizeof(cufftComplex), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_s2, sig2, 8 * sizeof(cufftComplex), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_s3, sig3, 8 * sizeof(cufftComplex), cudaMemcpyHostToDevice);
+
+    cufftHandle fftplan;
+    cufftPlan1d(&fftplan, 8, CUFFT_C2C, 1);
+    cufftExecC2C(fftplan, d_s1, d_s1, CUFFT_FORWARD);
+    cufftExecC2C(fftplan, d_s2, d_s2, CUFFT_FORWARD);
+    cufftExecC2C(fftplan, d_s3, d_s3, CUFFT_FORWARD);
+
+    cufftComplex fft1 = new cufftComplex[8];
+    cufftComplex fft3 = new cufftComplex[8];
+    cufftComplex fft3 = new cufftComplex[8];
+
+    cudaMemcpy(fft1, d_s1, 8 * sizeof(cufftComplex), cudaMemcpyHostToDevice);
+    cudaMemcpy(fft2, d_s2, 8 * sizeof(cufftComplex), cudaMemcpyHostToDevice);
+    cudaMemcpy(fft3, d_s2, 8 * sizeof(cufftComplex), cudaMemcpyHostToDevice);
+
+    cout << "Signal 1: " << endl;
+    for (int ii = 0; ii < 8; ii++) {
+        cout << sig1[ii].x << " + i*" << sig1[ii].y << endl;
+    }
+
+    cout << "Signal 1 FFT: " << endl;
+    for (int ii = 0; ii < 8; ii++) {
+        cout << fft1[ii].x << " + i*" << fft1[ii].y << endl;
+    }
+
+    cout << "Signal 2: " << endl;
+    for (int ii = 0; ii < 8; ii++) {
+        cout << sig2[ii].x << " + i*" << sig2[ii].y << endl;
+    }
+
+    cout << "Signal 2 FFT: " << endl;
+    for (int ii = 0; ii < 8; ii++) {
+        cout << fft2[ii].x << " + i*" << fft2[ii].y << endl;
+    }
+
+    cout << "Signal 3: " << endl;
+    for (int ii = 0; ii < 8; ii++) {
+        cout << sig3[ii].x << " + i*" << sig3[ii].y << endl;
+    }
+
+    cout << "Signal 1 FFT: " << endl;
+    for (int ii = 0; ii < 8; ii++) {
+        cout << fft3[ii].x << " + i*" << fft3[ii].y << endl;
+    }
+
+    cudaFree(d_s1);
+    cudaFree(d_s2);
+    cudaFree(d_s3);
+
+    delete[] sig1;
+    delete[] sig2;
+    delete[] sig3;
+    delete[] fft1;
+    delete[] fft2;
+    delete[] fft3;
+
+    return 0;
+}
